99924fa2b6ac9689e99a2971db771d2846ca36a2
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-11-15 09:52:45 -0800

    adds ManyToManyField to books

--- django/books_demo/apps/books/models.py
+++ django/books_demo/apps/books/models.py
@@ -35,3 +35,9 @@ class Book(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
+class Publisher(models.Model):
+	name = models.CharField(max_length=255)
+	books = models.ManyToManyField(Book, related_name="publishers")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
--- django/books_demo/apps/books/views.py
+++ django/books_demo/apps/books/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Author, Book
+from .models import Author, Book, Publisher
 
 # Create your views here.
 def index(request):
 	context = {
 		"authors": Author.objects.all(),
 		"books": Book.objects.all(),
+		"publishers": Publisher.objects.all(),
 	}
 
 	return render(request, "books/index.html", context)
@@ -25,4 +26,15 @@ def create_author(request):
 def create_book(request):
 	author = Author.objects.get(id=request.POST["author"])
 	Book.objects.create(title=request.POST["title"], rating=request.POST["rating"], author=author)
+	return redirect("index")
+
+def create_publisher(request):
+	Publisher.objects.create(name=request.POST["name"])
+	return redirect("index")
+
+def add_book_to_publisher(request):
+	print(request.POST)
+	book = Book.objects.get(id=request.POST["book_id"])
+	publisher = Publisher.objects.get(id=request.POST["publisher_id"])
+	publisher.books.add(book)
 	return redirect("index")
